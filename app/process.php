f98d2c9121983be53321a8ff0e28b9d2d0c59a26
Kun Akos <kunaakos@gmail.com>
2014-01-17 17:09:12 +0100

    basic Drive upload functionality

--- app/process.php
+++ app/process.php
@@ -4,22 +4,45 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 
-echo "Done.";
+$data = $email . "\n" . $phone;
 
-// require_once 'google-api-php-client/src/Google_Client.php';
-// require_once 'google-api-php-client/src/contrib/Google_DriveService.php';
+require_once 'google-api-php-client/src/Google_Client.php';
+require_once 'google-api-php-client/src/contrib/Google_DriveService.php';
 
-// // client_id, client_secret, redirect_uri, refresh_token
-// $stored = json_decode(file_get_contents('auth.json'));
+// get client_id, client_secret, redirect_uri, refresh_token
+$stored = json_decode(file_get_contents('auth.json'));
 
-// $client = new Google_Client();
+// AUTH!
+$client = new Google_Client();
+$client->setClientId($stored->client_id);
+$client->setClientSecret($stored->client_secret);
+$client->setRedirectUri($stored->redirect_uri);
+$client->setScopes(array('https://www.googleapis.com/auth/drive'));
+$client->refreshToken($stored->refresh_token);
 
-// $client->setClientId($stored->client_id);
-// $client->setClientSecret($stored->client_secret);
-// $client->setRedirectUri($stored->redirect_uri);
-// $client->setScopes(array('https://www.googleapis.com/auth/drive'));
-// $client->refreshToken($stored->refresh_token);
+// connect to Drive
+$service = new Google_DriveService($client);
 
-// $service = new Google_DriveService($client);
+// upload
+try {
+    $file = new Google_DriveFile();
+    $file->setTitle($name);
+    $file->setDescription("test");
+    $file->setMimeType('text/plain');
+    $createdFile = $service->files->insert(
+        $file, 
+        array(
+            'data' => $data,
+            'mimeType' => 'text/plain',
+        )
+    );
+
+    echo "Done.";  
+
+} catch (Exception $ex) {
+
+    echo "Error.";
+
+}
 
 ?>
